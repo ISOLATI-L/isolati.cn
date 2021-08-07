04f7bc1f5343db934239a2e284f8618fa7c3bf66
ISOLATI-L <isolati@qq.com>
2021-08-07 19:09:44 +0800

    modified:   .gitignore 	modified:   wwwroot/css/layout.css 	modified:   wwwroot/css/videos.css 	deleted:    wwwroot/js/layout.js 	modified:   wwwroot/layout.html 	modified:   wwwroot/videos.html

--- wwwroot/js/layout.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    let transition_noneItems=
-        document
-        .querySelectorAll(".transition_init");
-    for(let i = 0; i < transition_noneItems.length; i++){
-        transition_noneItems[i].classList.remove("transition_init");
-    }
-};
