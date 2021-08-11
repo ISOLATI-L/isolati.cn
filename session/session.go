2c9604c07ed624127223d3dd61c1edfa55b4ee3f
ISOLATI-L <isolati@qq.com>
2021-08-11 18:15:30 +0800

    modified:   session/fromMemory.go 	modified:   session/session.go

--- session/session.go
+++ session/session.go
@@ -1,7 +1,6 @@
 package session
 
 import (
-	"sync"
 	"time"
 )
 
@@ -16,57 +15,4 @@ type Session interface {
 	GetMaxAge() int64
 	SetMaxAge(age int64)
 	GetId() string
-}
-
-type SessionFromMemory struct {
-	sid              string
-	lock             sync.Mutex
-	lastAccessedTime time.Time
-	maxAge           int64
-	data             map[interface{}]interface{}
-}
-
-func newSessionFromMemory(sid string) *SessionFromMemory {
-	return &SessionFromMemory{
-		sid:    sid,
-		data:   make(map[interface{}]interface{}),
-		maxAge: DEFAULT_TIME,
-	}
-}
-
-func (si *SessionFromMemory) Set(key interface{}, value interface{}) {
-	si.lock.Lock()
-	defer si.lock.Unlock()
-	si.data[key] = value
-}
-
-func (si *SessionFromMemory) Get(key interface{}) interface{} {
-	return si.data[key]
-}
-
-func (si *SessionFromMemory) Remove(key interface{}) error {
-	if si.data[key] != nil {
-		delete(si.data, key)
-	}
-	return nil
-}
-
-func (si *SessionFromMemory) GetLastAccessedTime() time.Time {
-	return si.lastAccessedTime
-}
-
-func (si *SessionFromMemory) UpdateLastAccessedTime() {
-	si.lastAccessedTime = time.Now()
-}
-
-func (si *SessionFromMemory) GetMaxAge() int64 {
-	return si.maxAge
-}
-
-func (si *SessionFromMemory) SetMaxAge(age int64) {
-	si.maxAge = age
-}
-
-func (si *SessionFromMemory) GetId() string {
-	return si.sid
 }
