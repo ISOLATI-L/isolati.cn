bd4fc3107340e791bb8c8c3f857a0d0e9fa62a08
ISOLATI-L <isolati@qq.com>
2021-08-12 23:37:27 +0800

    modified:   controller/robots.go

--- controller/robots.go
+++ controller/robots.go
@@ -12,22 +12,22 @@ var robotsHandler = http.FileServer(http.Dir(global.ROOT_PATH + "wwwroot"))
 func logReferer(r *http.Request) {
 	userAgent := r.UserAgent()
 	log.Println("Visited robots.txt: ", userAgent)
-	// result, err := global.DB.Exec(
-	// 	`INSERT INTO robots (RuserAgent) VALUES (?);`,
-	// 	userAgent,
-	// )
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// 	return
-	// }
-	// affected, err := result.RowsAffected()
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// 	return
-	// }
-	// if affected == 0 {
-	// 	log.Println(result)
-	// }
+	result, err := global.DB.Exec(
+		`INSERT INTO robots (RuserAgent) VALUES (?);`,
+		userAgent,
+	)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	if affected == 0 {
+		log.Println(result)
+	}
 }
 
 func handleRobots(w http.ResponseWriter, r *http.Request) {
