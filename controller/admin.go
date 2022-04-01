ca49c59b59c125e85f5b972d23676e48fa5f792f
ISOLATI-L <isolati@qq.com>
2022-04-01 22:47:00 +0800

    modified:   controller/admin.go 	modified:   controller/login.go

--- controller/admin.go
+++ controller/admin.go
@@ -12,39 +12,39 @@ import (
 var adminTemplate = template.New("admin")
 
 func showAdminPage(w http.ResponseWriter, r *http.Request) {
-	adminTemplate.ExecuteTemplate(w, "layout", layoutMsg{
-		PageName: "admin",
-		ContainerData: sliderContainerData{
-			LeftSliderData:  global.LEFT_SLIDER,
-			RightSliderData: global.RIGHT_SLIDER,
-			ContentData:     nil,
-		},
-	})
+	vIdentity, err := session.UserSession.GetByRequest(r, "identity")
+	if err != nil {
+		if err == session.ErrNoCookies {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	} else {
+		var identity string
+		err = json.Unmarshal(vIdentity, &identity)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			if identity != "admin" {
+				http.Redirect(w, r, "/login", http.StatusSeeOther)
+			} else {
+				adminTemplate.ExecuteTemplate(w, "layout", layoutMsg{
+					PageName: "admin",
+					ContainerData: sliderContainerData{
+						LeftSliderData:  global.LEFT_SLIDER,
+						RightSliderData: global.RIGHT_SLIDER,
+						ContentData:     nil,
+					},
+				})
+			}
+		}
+	}
 }
 
 func handleAdmin(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		vIdentity, err := session.UserSession.GetByRequest(r, "identity")
-		if err != nil {
-			if err == session.ErrNoCookies {
-				http.Redirect(w, r, "/login", http.StatusSeeOther)
-			} else {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-		} else {
-			var identity string
-			err = json.Unmarshal(vIdentity, &identity)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			} else {
-				if identity != "admin" {
-					http.Redirect(w, r, "/login", http.StatusSeeOther)
-				} else {
-					showAdminPage(w, r)
-				}
-			}
-		}
+		showAdminPage(w, r)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
--- controller/login.go
+++ controller/login.go
@@ -16,6 +16,22 @@ import (
 var loginTemplate = template.New("login")
 
 func showLoginPage(w http.ResponseWriter, r *http.Request) {
+	for {
+		vIdentity, err := session.UserSession.GetByRequest(r, "identity")
+		if err != nil {
+			break
+		}
+		var identity string
+		err = json.Unmarshal(vIdentity, &identity)
+		if err != nil {
+			break
+		}
+		if identity != "admin" {
+			break
+		}
+		http.Redirect(w, r, "/admin", http.StatusSeeOther)
+		return
+	}
 	loginTemplate.ExecuteTemplate(w, "layout", layoutMsg{
 		PageName: "login",
 		ContainerData: sliderContainerData{
@@ -27,60 +43,48 @@ func showLoginPage(w http.ResponseWriter, r *http.Request) {
 	)
 }
 
+func postLoginRequest(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Login Request:", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Println("Login Request:", string(data))
+	row := db.DB.QueryRow(
+		`SELECT md5password FROM admins
+		WHERE md5password=?;`,
+		string(data),
+	)
+	var md5password string
+	err = row.Scan(&md5password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println("Error Password")
+			w.WriteHeader(http.StatusUnauthorized)
+		} else {
+			log.Println("Query Fail:", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	} else {
+		sid := session.UserSession.BeginSession(w, r)
+		err = session.UserSession.Set(sid, "identity", "admin")
+		if err != nil {
+			log.Println("Set Identity Fail:", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			log.Println("Login Success")
+			w.WriteHeader(http.StatusOK)
+		}
+	}
+}
+
 func handleLogin(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		vIdentity, err := session.UserSession.GetByRequest(r, "identity")
-		if err != nil {
-			showLoginPage(w, r)
-			break
-		}
-		var identity string
-		err = json.Unmarshal(vIdentity, &identity)
-		if err != nil {
-			showLoginPage(w, r)
-			break
-		}
-		if identity != "admin" {
-			showLoginPage(w, r)
-			break
-		}
-		http.Redirect(w, r, "/admin", http.StatusSeeOther)
-		break
+		showLoginPage(w, r)
 	case http.MethodPost:
-		data, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Println("Login Request:", err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			break
-		}
-		log.Println("Login Request:", string(data))
-		row := db.DB.QueryRow(
-			`SELECT md5password FROM admins
-			WHERE md5password=?;`,
-			string(data),
-		)
-		var md5password string
-		err = row.Scan(&md5password)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				log.Println("Error Password")
-				w.WriteHeader(http.StatusUnauthorized)
-			} else {
-				log.Println("Query Fail:", err.Error())
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-		} else {
-			sid := session.UserSession.BeginSession(w, r)
-			err = session.UserSession.Set(sid, "identity", "admin")
-			if err != nil {
-				log.Println("Set Identity Fail:", err.Error())
-				w.WriteHeader(http.StatusInternalServerError)
-			} else {
-				log.Println("Login Success")
-				w.WriteHeader(http.StatusOK)
-			}
-		}
+		postLoginRequest(w, r)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
