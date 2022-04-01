96b65bc1da2e22081ddc0c17654188f223d0baea
ISOLATI-L <isolati@qq.com>
2022-04-02 03:09:11 +0800

    modified:   wwwroot/js/writing.js 	modified:   wwwroot/writing.html

--- wwwroot/js/writing.js
+++ wwwroot/js/writing.js
@@ -1,3 +1,10 @@
+var head = document.getElementsByTagName('head')[0];
+var editormdCss = document.createElement('link');
+editormdCss.href = "editormd/css/editormd.min.css";
+editormdCss.rel = "stylesheet";
+editormdCss.type = "text/css";
+head.appendChild(editormdCss);
+
 var markdown_editor = editormd("markdown_editor", {
     width: "100%",
     height: "100%",
